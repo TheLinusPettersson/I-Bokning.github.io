178175f81c61442df9a08235a8d9263aa8366b8a
Linus Pettersson <93656671+TheLinusPettersson@users.noreply.github.com>
2024-07-06 15:43:36 +0200

    Updates

--- assets/NotFound-55aa16a5.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-8e16c648.js";const n=()=>o.jsx("div",{children:"404 page not found"});export{n as default};
---
+++ assets/NotFound-c3b6b440.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-dc6755eb.js";const n=()=>o.jsx("div",{children:"404 page not found"});export{n as default};
