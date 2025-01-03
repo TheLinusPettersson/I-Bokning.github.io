87092afeba07d37c1d63c19803ce217f7a8852fc
Linus Pettersson <93656671+TheLinusPettersson@users.noreply.github.com>
2025-01-03 15:07:35 +0100

    Updates

--- assets/InfoCard-abc1875b.js
+++ assets/InfoCard-abc1875b.js
@@ -1,4 +1,4 @@
-import{g as f,j as n,b as m,L as u}from"./index-bfac9f14.js";var d={exports:{}};/*!
+import{g as f,j as n,b as m,L as u}from"./index-5ccd8941.js";var d={exports:{}};/*!
 	Copyright (c) 2018 Jed Watson.
 	Licensed under the MIT License (MIT), see
 	http://jedwatson.github.io/classnames
